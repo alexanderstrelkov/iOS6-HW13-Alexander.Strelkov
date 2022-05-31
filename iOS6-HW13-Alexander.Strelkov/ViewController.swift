fa876bf84c5ab4a3e64682d828dbd1622e74e562
Alex <wild6158@gmail.com>
2022-05-31 22:21:55 +0300

    Created basic UITableView.

--- iOS6-HW13-Alexander.Strelkov/ViewController.swift
+++ iOS6-HW13-Alexander.Strelkov/ViewController.swift
@@ -7,13 +7,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct SettingsOptions {
+    let title: String
+    let icon: UIImage?
+    let iconColor: UIColor
+    let handler: (() -> Void)
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //MARK: -TableView
+    
+    private var tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return table
+    }()
+    
+    var data = [String]()
+    var models = [SettingsOptions]()
+    
+    //MARK: -viewLoads:
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        for x in 0...10 {
+//            data.append("Data \(x)")
+//        }
+        config()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    //MARK: -Functions
+    
+    func config() {
+        self.models = Array(0...50).compactMap({
+            SettingsOptions(title: "Item \($0)", icon: UIImage(systemName: "house"), iconColor: .systemPink) {
+            }
+        })
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return data.count
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.text = model.title
+        return cell
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("cell tapped")
+    }
+    
 
 }
 
